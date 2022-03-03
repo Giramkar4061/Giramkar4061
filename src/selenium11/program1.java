a2c2670de3d68c3691f237f43c4cb9bf0e0d702f
DELL <DELL@DESKTOP-997ERFS>
2022-03-03 16:07:06 +0530

    comm1

---
+++ src/selenium11/program1.java
@@ -0,0 +1,10 @@
+package selenium11;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiiii");
+
+	}
+
+}
---
+++ src/selenium11/program2.java
@@ -0,0 +1,12 @@
+package selenium11;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
